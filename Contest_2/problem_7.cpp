e30e7d93cca724441d6f0c739b7c6eac22d83c65
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-25 20:15:33 +0600

    problem 7

--- Contest_2/problem_7.cpp
+++ Contest_2/problem_7.cpp
@@ -1,6 +1,40 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int s1, s2, s3; cin >> s1 >> s2 >> s3;
+//     int count = 0;
+//     if(s1 == 1){
+//         count++;
+//     }
+//     if(s2 == 1){
+//         count++;
+//     }
+//     if(s3 == 1){
+//         count++;
+//     }
+//     cout << count <<endl;
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n; cin >> n;
+int main()
+{
+    int n;
+    cin >> n;
+    int count = 0;
+    if (n % 10 == 1)
+    {
+        count++;
+    }
+    if (n % 100 == 1)
+    {
+        count++;
+    }
+    if (n % 1000 == 1)
+    {
+        count++;
+    }
+    cout << count << endl;
     return 0;
 }
