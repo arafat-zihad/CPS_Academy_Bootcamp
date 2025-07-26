600bde926b481b39e57cc55a256fb5aadc8af85f
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-26 16:52:11 +0600

    problem 4 solved

--- Contest_2/problem_3.cpp
+++ Contest_2/problem_3.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main(){
     int x, y; cin >> x >> y;
     int a = x - y;
+    int b = y - x;
     // abs(a);
-    if(abs(a) <= 2 && y - x <= 3) cout << "Yes" << endl;
+    if(x < y && abs(a) <= 2) cout << "Yes" << endl;
+
+    else if(x > y && abs(b) <= 3) cout << "Yes" << endl;
     else cout <<"No" << endl;
     return 0;
 }
