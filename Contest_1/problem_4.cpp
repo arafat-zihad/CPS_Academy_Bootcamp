b37d18ff9d09600d6be5246be46b18e48becad60
arafat-zihad <arafat.zihad01@gmail.com>
2025-07-27 23:42:19 +0600

    contest 1 prob 4

--- Contest_1/problem_4.cpp
+++ Contest_1/problem_4.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    cout << "hello";
+    int a; cin >> a;
+    int b; cin >> b;
+
+    int stam = a / b;
+    cout << stam;
     return 0;
 }
