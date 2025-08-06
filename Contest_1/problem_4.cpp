0fabd60130503180b357be9b6d035a3dc687c503
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-06 22:13:51 +0600

    first attempt

--- Contest_1/problem_4.cpp
+++ Contest_1/problem_4.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int a; cin >> a;
-    int b; cin >> b;
+    int st; cin >> st;
+    int p; cin >> p;
 
-    int stam = a / b;
-    cout << stam;
+    int count = 0;
+    for(int i = 1; i >= 0; i++){
+
+        int a = p * i;
+        if(a > st){
+            break;
+        }
+        count++;
+    }
+    cout << count+1;
     return 0;
 }
