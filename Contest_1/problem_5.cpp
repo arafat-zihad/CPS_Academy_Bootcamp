6d1c6db5d8d99d4190022f1e95087ae9ada7360f
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-07 21:14:55 +0600

    first attempt

--- Contest_1/problem_5.cpp
+++ Contest_1/problem_5.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int a, b;
+    cin >> a >> b;
+
+    int area = (a * b);
+    int P = 2 * (a+b);
+
+    cout << area << endl;
+    cout << P << endl;
     
     return 0;
 }
