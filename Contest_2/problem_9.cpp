39613863ca70213a93686a5f065a4a63f74a3041
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-08 19:28:53 +0600

    problem 9 solved

--- Contest_2/problem_9.cpp
+++ Contest_2/problem_9.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int n; cin >> n;
-    while (n /=10){
-        cout << n << endl;
+    int n, a; cin >> n >> a;
+    int r = n / 500;
+    int rest = n % 500;
+    
+    if(rest <= a ){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
     }
     return 0;
 }
