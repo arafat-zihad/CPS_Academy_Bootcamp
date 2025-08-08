622f5587c39f3e8bbc07928ab341a0dab7bbc0b3
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-08 20:25:16 +0600

    problem 10 solve 2nd attampt

--- Contest_2/problem_10.cpp
+++ Contest_2/problem_10.cpp
@@ -4,16 +4,7 @@
 using namespace std;
 int main(){
     int n; cin >> n;
-    int r;
-    if(n % 3 == 0){
-        r = n / 3;
-    }
-    if( n % 5== 0){
-        r = n / 5;
-    }
-    if(n <= 2){
-        r = 0;
-    }
-    cout << r;
+    int g = n / 3;
+    cout << g;
     return 0;
 }
