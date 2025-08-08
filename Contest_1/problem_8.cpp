b090f5fed70ce27fa6d523b40c56495de94b45d3
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-08 14:30:06 +0600

    method 2 solved

--- Contest_1/problem_8.cpp
+++ Contest_1/problem_8.cpp
@@ -1,9 +1,24 @@
 //Bismillah
+
+//Method 1
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a, b; cin >> a >> b;
+//     int r = pow(a,b);
+//     cout << r << endl;
+//     return 0;
+// }
+
+//Method 2
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int a, b; cin >> a >> b;
-    int r = pow(a,b);
-    cout << r << endl;
+int a, b; cin >> a >> b;
+    int ans = 1;
+    for(int i = 0; i < b; i++){
+        ans = ans * a;
+    }
+    cout << ans << endl;
     return 0;
 }
