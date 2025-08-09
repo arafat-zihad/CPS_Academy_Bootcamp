13ff5f08a1855c672f7663641d6f81f6da4b34dc
arafat-zihad <arafat.zihad01@gmail.com>
2025-08-09 22:04:46 +0600

    problem 3 solved

--- Contest_3/problem_3.cpp
+++ Contest_3/problem_3.cpp
@@ -1,10 +1,14 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
-    int x; cin >> x; 
-    for(int i =1; i <= x; i++){
-        
-        cout << "L" << "ong";
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "L";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "o";
     }
+    cout << "ng" << endl;
     return 0;
 }
